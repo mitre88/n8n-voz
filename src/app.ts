3168cd4fd2d9f859311d49e1eb233284e130794c
Alex Mitre <bedr10_capacitacion@hotmail.com>
2024-03-29 19:09:37 -0600

    gpt-3.5-turbo-0125

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import flow from './flows';
 import { provider } from './provider';
 
 const PORT = process.env.PORT ?? 3001
-const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-4-0125-preview')
+const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-3.5-turbo-0125')
 
 const main = async () => {
     const { httpServer } = await createBot({
